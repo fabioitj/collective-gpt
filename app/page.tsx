54a7bba2a8bbde9a49164bf4b6b155f3a5990e71
Fabio <fridow8@gmail.com>
2023-07-06 02:18:27 -0300

    tesdte
    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image"
-import Background from "./../public/openai-working-humanoid-robot.jpg"
-
 function WhatsappIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256" width="32px" height="32px" fill-rule="nonzero">
